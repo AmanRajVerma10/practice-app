78c4c2ed8e052a3306bb4801076ae2258623d660
AmanRajVerma10 <verma.aman464@gmail.com>
2024-06-29 15:25:14 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const submitHandler = () => {
+    const copyTasks = [...tasks];
+    copyTasks.push(task);
+    setTasks(copyTasks);
+    setTask("");
+  };
+
+  function deleteHandler(i){
+    console.log(i);
+  }
+
   return (
-    <div className="App">
+    <div>
       <h1>This is a practice arena!</h1>
-      <div>{logo}</div>
+      <input
+        placeholder="Enter Task"
+        onChange={(e) => {
+          setTask(e.target.value);
+        }}
+      ></input>
+      <button onClick={submitHandler}>Add Task</button>
+      <ul>
+        {tasks.map((t, i) => (
+          <li key={i}>{t} <button onClick={deleteHandler.bind(null,i)}>Delete</button></li>
+        ))}
+      </ul>
     </div>
   );
 }
