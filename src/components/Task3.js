303eb180f9abc6c534dfad72e32ee01e0861c6f7
AmanRajVerma10 <verma.aman464@gmail.com>
2024-11-05 00:49:54 +0530

    modifications

--- src/components/Task3.js
+++ src/components/Task3.js
@@ -2,26 +2,37 @@ import React, { useState } from "react";
 
 function Task3() {
   const [counter, setCounter] = useState(0);
+  const [val, setVal] = useState(0);
 
   const incrementHandler = () => {
     setCounter((prevValue) => {
-      return prevValue + 1;
+      return prevValue + val;
     });
   };
 
   const decrementHandler = () => {
     setCounter((prevValue) => {
-      return prevValue - 1;
+      return prevValue - val;
     });
   };
 
   return (
     <div>
       <h1>{counter}</h1>
+      <label>Increment/Decrement by</label>
+      <input
+        type="number"
+        value={val}
+        onChange={(e) => {
+          const inputValue = +e.target.value;
+          setVal(inputValue >= 0 ? inputValue : 0);
+        }}
+      ></input>
       <button onClick={incrementHandler}>Increment</button>
       <button
         onClick={() => {
           setCounter(0);
+          setVal(0);
         }}
       >
         Reset
