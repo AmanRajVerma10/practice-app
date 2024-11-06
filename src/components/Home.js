3aae2934ce8b8932ee43289d90334e93ba4ee7e0
AmanRajVerma10 <verma.aman464@gmail.com>
2024-11-07 01:21:03 +0530

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,65 @@
 import React from "react";
-import './Home.css';
+import "./Home.css";
 const Home = () => {
-  return <div><h1>Practice Arena</h1></div>;
+  return (
+    <div>
+      <h1>Practice Arena</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus,
+        dolor a congue vulputate, sem lectus suscipit libero, quis finibus ante
+        mi nec augue. Sed aliquet sit amet ipsum at sagittis. Sed tristique
+        cursus elit, a consequat dolor iaculis at. Nulla vel lacus lorem.
+        Praesent nibh ex, tincidunt nec pellentesque at, euismod tempus ligula.
+        In fringilla commodo lacus a viverra. Aliquam eget finibus quam, nec
+        rhoncus nisl. Sed ullamcorper rhoncus tortor nec commodo. Sed dignissim
+        euismod turpis, vel lacinia turpis aliquet vel. Nullam tincidunt sit
+        amet tellus at fringilla. Pellentesque non pulvinar purus. In in
+        vehicula arcu. Suspendisse maximus porta hendrerit. Phasellus eu rhoncus
+        ex. In hac habitasse platea dictumst. Sed ac felis quis urna mattis
+        finibus vel et ipsum. Cras gravida tristique feugiat. Sed tincidunt est
+        velit, ac viverra risus ultrices non. Mauris consequat neque sit amet
+        tincidunt congue. Phasellus quis orci ac orci ultricies tempus. Integer
+        cursus arcu ligula, accumsan eleifend nunc mollis a. Vivamus hendrerit
+        malesuada vulputate. Nunc et condimentum nisi. Ut faucibus odio eget
+        sodales mollis. Phasellus imperdiet augue tortor. Cras ornare egestas
+        orci, quis feugiat sapien congue sed. Nam ultrices est non cursus
+        semper. Maecenas et eros bibendum, blandit ante vel, molestie nunc.
+        Proin a leo in lorem tristique bibendum lacinia mattis felis. Nullam nec
+        arcu ac sem mollis molestie vitae eget velit. Aliquam egestas viverra
+        augue sit amet iaculis. Fusce nec mi mauris. Aenean euismod efficitur
+        tempor. Duis cursus vehicula velit nec aliquam. Morbi justo leo, tempus
+        eget arcu eu, maximus scelerisque libero. Duis euismod lacus a augue
+        volutpat, eu suscipit quam mollis. Nunc lacinia venenatis maximus. Etiam
+        nec augue mi. Duis lacinia leo non feugiat malesuada. Proin volutpat at
+        lorem sit amet ullamcorper. Fusce ut enim vitae libero dignissim
+        fringilla sed vitae justo. Sed in dui a lectus posuere tristique. Fusce
+        condimentum, orci in interdum blandit, justo leo aliquet ligula, quis
+        fringilla justo quam vel augue. Praesent lacinia ante quis molestie
+        porta. Ut luctus, dui non vestibulum pulvinar, odio dui tincidunt nunc,
+        sit amet ultrices orci metus vitae ex. Nullam dictum massa nibh, eu
+        sollicitudin erat pretium nec. Integer congue arcu nunc, sit amet
+        placerat enim vestibulum et. Praesent quis egestas odio. Donec ut risus
+        mauris. Proin fermentum venenatis augue quis sollicitudin. Quisque porta
+        eget velit sit amet blandit. Quisque massa nibh, pretium vitae nisl
+        eget, feugiat gravida enim. Maecenas sed augue rhoncus, efficitur elit
+        id, commodo dolor. Cras feugiat erat nec placerat condimentum. Vivamus
+        ipsum est, vehicula eu nulla sed, cursus porttitor tellus. Phasellus
+        felis purus, hendrerit eget turpis in, cursus suscipit ante. Donec id
+        erat in nisi consectetur vulputate a id lacus. Maecenas varius mauris at
+        felis finibus efficitur. Aliquam volutpat purus non magna ultrices
+        dictum. Praesent accumsan, ante et porttitor hendrerit, orci nulla
+        placerat arcu, sit amet malesuada felis lorem a ligula. Nulla quis
+        venenatis leo. Pellentesque eu quam ipsum. Nunc placerat quis dui at
+        pretium. Phasellus facilisis tortor id ullamcorper sodales. Aliquam
+        dictum tellus facilisis dui pellentesque, vel accumsan enim varius.
+        Suspendisse et ipsum porta, tincidunt libero quis, posuere lectus.
+        Maecenas aliquam pharetra tortor, non tincidunt nisi venenatis luctus.
+        Maecenas quis leo vel mauris rutrum finibus ac non libero. Sed interdum
+        orci nunc, sed pulvinar turpis dignissim id. Curabitur eget orci nunc.
+        Mauris eu laoreet erat.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -4,14 +4,14 @@ import './MainNavigation.css'
 const MainNavigation = (props) => {
 
   return (
-    <nav>
+    <div className="navigation">
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/task1'>Task 1</Link></li>
         <li><Link to='/task2'>Task 2</Link></li>
         <li><Link to='/task3'>Task 3</Link></li>
       </ul>
-    </nav>
+    </div>
   );
 };
 
